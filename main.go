3c954cc4c3958d17272d60a8f009904bc242813c
Sammy <sammy-jo.wymer@diconium.com>
2026-04-13 13:28:59 +0200

    feat(notifications): send notifications to discord

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
 	"strings"
 
 	"github.com/gen2brain/beeep"
@@ -19,11 +23,37 @@ func addColourToHeadline(headline string) string {
 	return headline
 }
 
+func sendToDiscord(webhookURL, title, headline, link string) {
+	// Creates a nice-looking card in Discord
+	payload := map[string]interface{}{
+		"embeds": []map[string]interface{}{
+			{
+				"title":       title,
+				"description": headline + "\n\n[Read Article](" + link + ")",
+				"color":       15158272, // LFC Red
+				"thumbnail": map[string]string{
+					"url": "https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Liverpool_FC.svg/1200px-Liverpool_FC.svg.png",
+				},
+			},
+		},
+	}
+
+	jsonData, _ := json.Marshal(payload)
+	http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonData))
+}
+
 func main() {
 
 	newsSource := []NewsSource{
 		{Name: "BBC Sport", Link: "https://www.bbc.com/sport/football/teams/liverpool"},
 		{Name: "Liverpool Echo", Link: "https://www.liverpoolecho.co.uk/all-about/liverpool-fc"},
+	}
+
+	webhookURL := os.Getenv("DISCORD_WEBHOOK_URL")
+
+	if webhookURL == "" {
+		fmt.Println("❌ Error: DISCORD_LFC_WEBHOOK not found in environment!")
+		return
 	}
 
 	if news := ScrapeLiverpoolNews(newsSource); len(news) > 0 {
@@ -34,6 +64,8 @@ func main() {
 			// TRIGGER POP-UP:
 			if strings.Contains(strings.ToLower(item.Headline), "gossip") ||
 				strings.Contains(strings.ToLower(item.Headline), "transfer") {
+
+				sendToDiscord(webhookURL, "🚨 LFC Transfer Gossip", item.Headline, item.Link)
 
 				err := beeep.Notify(
 					"LFC Scout: "+item.Source, // Title
